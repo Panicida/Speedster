d9c7c56ec310020a345c34845994e45d4fdbd05c
Panicida <javiropos@gmail.com>
2018-05-10 19:01:10 +0200

    Project architecture

---
+++ Code/src/Motor Prototype/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Motor Prototype.cpp
+ *
+ * Created: 10/05/2018 18:55:22
+ * Author : javir
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
---
+++ Code/src/Motor/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * Motor.cpp
+ *
+ * Created: 10/05/2018 18:57:08
+ * Author : javir
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
