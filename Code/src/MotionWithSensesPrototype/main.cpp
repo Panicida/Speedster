e65ed0d4e44a88836e5e287a7a0bb6c3e4c033af
Panicida <javiropos@gmail.com>
2018-05-14 22:09:23 +0200

    MotionWithSensesPrototype finished

--- Code/src/MotionWithSensesPrototype/main.cpp
+++ Code/src/MotionWithSensesPrototype/main.cpp
@@ -11,10 +11,27 @@
 
 int main(void)
 {
+	// Create sensor on ADC7, potentiometer.
+	unsigned char potPin[1] = { ADC7_PIN };
+	Senses::QTRASensor pot(potPin, 1, 1);
 	
+	// Create motor control for PWM1
+	Motion::MotorInfoStruct motorInfoStruct;
+	Motion::Motors::SetMotorInfo(&motorInfoStruct, Motion::Left, Motion::TC0);
 	
+	Motion::MotorsComplex motor(&motorInfoStruct, 1);
+	
+	pot.EmittersOn();
     while (1) 
     {
+		unsigned int speed;
+		pot.Read(&speed);
+		
+		Motion::MotorSpeedStruct speedStruct;
+		speedStruct.pwmPin = Motion::TC0;
+		speedStruct.speed = speed;
+		
+		motor.SetSpeed(&speedStruct);
     }
 }
 
