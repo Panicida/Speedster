0a38c4e4407600d9ff671d7f2aebe589781c9e7f
Panicida <javiropos@gmail.com>
2018-05-23 20:23:41 +0200

    In progress
    Added code example for simple line follower

--- Code/src/Control/PidControl.h
+++ Code/src/Control/PidControl.h
@@ -21,3 +21,45 @@ namespace Control
 }
 
 #endif /* PIDCONTROL_H_ */
+
+/*
+void loop ()
+{
+	unsigned int valorSensores [4];
+	
+	unsigned int posicion = sensoresReflexion.readLine(valorSensores);
+	
+	seguirLinea (posicion);
+}
+
+void seguirLinea (int posicion)
+{
+	int error = posicion - 1500;
+	
+	int velocidadDiferencial = error / 4 + 6 * (error - ultimoError);
+	ultimoError = error;
+	
+	int motorI = MAX_SPEED + velocidadDiferencial;
+	int motorD = MAX_SPEED - velocidadDiferencial;
+	
+	if (motorI > MAX_SPEED)
+	{
+		motorI = MAX_SPEED;
+	}
+	if (motorD > MAX_SPEED)
+	{
+		motorD = MAX_SPEED;
+	}
+	if (motorI < -MAX_SPEED)
+	{
+		motorI = -MAX_SPEED;
+	}
+	if (motorD < -MAX_SPEED)
+	{
+		motorD = -MAX_SPEED;
+	}
+	
+	velocidad(motorI,motorD);
+}
+
+*/
